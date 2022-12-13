2b8cc31fd04350a9d203b4334118918d5baae67b
priyanshpatil11 <117080904+priyanshpatil11@users.noreply.github.com>
2022-12-13 16:48:53 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket-2"
+    bucket = "sagar-patil-1"
     key = "main"
     region = "ap-south-1"
-    dynamodb_table = "my-dynamodb-table"
+    dynamodb_table = "sagar-dynamo-1"
   }
 }
